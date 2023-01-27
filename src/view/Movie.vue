47cd8303eecb1d7ad7c8c6fa2cdbd78e084a2394
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 04:25:26 -0600

    a
    a

--- src/view/Movie.vue
+++ src/view/Movie.vue
@@ -1,7 +1,13 @@
 <script setup>
 import { useRouter } from "vue-router"
+import { onMounted, ref } from "vue";
 const router = useRouter()
 const movieId = router.currentRoute.value.params.id
+const movie=ref(null)
+onMounted(async() => {
+movie.value = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=c46280d39e34b3012975df9f8e6e9e70`).then(res => res.json())
+
+})
 </script> 
 <template>
 Movie
