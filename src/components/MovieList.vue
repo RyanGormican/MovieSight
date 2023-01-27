59f66a51dc6c6b31f8e643078390dbc2890f7c01
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 03:23:57 -0600

    a
    a

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,12 +1,14 @@
 <script setup>
+import MovieCard from './MovieCard.vue';
 const { movies } = defineProps({
 movies: Array
 })
 </script>
 <template>
 <div class ="grid grid-cols-3 gap-4 mx-20 my-10">
-	<div v-for="movie in movies">
-		{{ movie.title }}
-	</div>
+	<MovieCard
+	 v-for="movie in movies"
+	:movie="movie"
+	/>
 </div>
 </template>
