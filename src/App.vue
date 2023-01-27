97d0071e616a174857487731cb924832e3137d9f
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 01:36:46 -0600

    a
    a

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-import { ref, onMounted } from "vue"
+  import { ref, onMounted } from "vue"
+  const movies = ref()
+  const getMovies = async () => {
+  await fetch("https://api.themoviedb.org/3/movie/550?api_key=c46280d39e34b3012975df9f8e6e9e70")
+  }
 </script>
-
 <template>
   <h1> Hello World</h1>
 </template>
