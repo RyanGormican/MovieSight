041d6f643d658b45f4b5e6e190526d98d7e6e8c5
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 05:53:39 -0600

    a
    a

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import Movie from "../view/Movie.vue"
 import { createRouter, createWebHistory  } from "vue-router"
 export const routes = [
 	{ path: "/", component: Home },
-	{ path: "/movies/:id", component: Movie },
+	{ path: "/movies/:id", component: Movie }
 ]
 export const router = createRouter({
 history: createWebHistory(),
