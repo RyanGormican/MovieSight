4089de0972a460dbdff51ac4db88aa8017bebcea
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 05:55:47 -0600

    a
    a

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
 import { RouterLink }from 'vue-router';
 </script> 
 <template>
-<nav class="flex text-white items-center gap-20 absolute left-0 right-0 mx-6 py-4 top-4 bg-transparent border-b-2 border-neutral-400">
+<nav class="flex z-10  text-white items-center gap-20 absolute left-0 right-0 mx-6 py-4 top-4 bg-transparent border-b-2 border-neutral-400">
 	<RouterLink to="/" class="flex text-3xl items-center font-bold gap-2">
 	<span> MovieSight</span>
 	</RouterLink>
