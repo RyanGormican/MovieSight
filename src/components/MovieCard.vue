52253918a1f0dc8dbd22e6f09be1773b5351c9d5
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 03:25:47 -0600

    a
    a

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,5 +1,16 @@
 <script setup>
+import getImage from '../lib/getImage';
+import { Icon } from '@iconify/vue';
 const { movie } = defineProps(["movie"])
+const {
+	title,
+	overview,
+	backdrop_path: background,
+	poster_path: poster,
+	release_date,
+	vote_average: vote,
+	original language: language
+} = movie
 </script>
 
 <template>
