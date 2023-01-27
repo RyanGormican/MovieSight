7eadc71a8b0f4e6c83de5d7c55401bc52215356c
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 01:59:42 -0600

    a
    a

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,25 @@
   import { ref, onMounted } from "vue"
   const movies = ref([])
   const bannerMovie = ref(null)
+  
   const findMovies = async () => {
-   movies.value = await fetch("https://api.themoviedb.org/3/movie/550?api_key=c46280d39e34b3012975df9f8e6e9e70")
+   movies.value = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=c46280d39e34b3012975df9f8e6e9e70")
   .then(res => res.json())
-  .then(res =>res.results)
+  .then(res => res.results)
  }
- const Random = (min, max) => {
+ 
+ const findRandom = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min)
  }
+ 
  onMounted(async() => {
  await findMovies()
- bannerMovie.value = movies.value[Random(0, movies.value.length-1)]
+ bannerMovie.value = movies.value[findRandom(0, movies.value.length - 1)]
  })
+ 
 </script>
 <template>
   <h1> Hello World</h1>
+  {{ bannerMovie }}
 </template>
 
