61211bdd2ce57d7cede33f717a0b321fadf8223b
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 16:54:21 -0600

    a
    a

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -7,20 +7,18 @@
   const AsyncBanner = defineAsyncComponent(() => {
   return import("../components/Banner.vue")
   })
-  const findMovies = async () => {
   movies.value = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=c46280d39e34b3012975df9f8e6e9e70")
   .then(res => res.json())
   .then(res => res.results)
-  }
+ 
 
   const findRandom = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min)
   }
 
-  onBeforeMount(async() => {
-  await findMovies()
+
   bannerMovie.value = movies.value[findRandom(0, movies.value.length - 1)]
-  })
+  
 
 </script>
 
