062b914c75bec88935aeca60483c0390f34a0c44
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 02:10:29 -0600

    a
    a

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <script setup>
   import { ref, onMounted, defineAsyncComponent } from "vue"
-  import Banner from "./components/Banner.vue"
   const movies = ref([])
   const bannerMovie = ref(null)
-  
+
   const AsyncBanner = defineAsyncComponent(() => {
-    return Banner
+  return "./components/Banner.vue"
   })
   const findMovies = async () => {
-   movies.value = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=c46280d39e34b3012975df9f8e6e9e70")
+  movies.value = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=c46280d39e34b3012975df9f8e6e9e70")
   .then(res => res.json())
   .then(res => res.results)
- }
- 
- const findRandom = (min, max) => {
+  }
+
+  const findRandom = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min)
- }
- 
- onMounted(async() => {
- await findMovies()
- bannerMovie.value = movies.value[findRandom(0, movies.value.length - 1)]
- })
- 
+  }
+
+  onMounted(async() => {
+  await findMovies()
+  bannerMovie.value = movies.value[findRandom(0, movies.value.length - 1)]
+  })
+
 </script>
 <template>
   <AsyncBanner
