3049c4282b7081af439066a85f09ec4ec7692543
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 06:16:06 -0600

    a
    a

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import Home from "../view/Home.vue"
 import Movie from "../view/Movie.vue"
 import { createRouter, createWebHistory  } from "vue-router"
-export const routes = [
+ const routes = [
 	{ path: "/", component: Home },
 	{ path: "/movies/:id", component: Movie }
 ]
