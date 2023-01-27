aac50d36d8b4fdab935ff9749589b01e3ad299b1
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 03:08:25 -0600

    a
    a

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -24,10 +24,11 @@ class="w-screen h-screen"
 <div class= "text-white p-10 flex flex-col justify-center w-full h-full bg-gradient-to-r from-black to-ransparent""
 <h1 class ="text-8xl font-bold"> {{title}} </h1>
 <p class ="mt-2 w-1/2 text-sm text-neutral-400"> {{ description }}</p>
-</div>
 <button class = "flex items-center gap-1 px-6 py-4 rounded-md bg-green-700 w-fit mt-4 transition hover:bg-neutral-800 font-semibold">
 <span>View More</span>
-<span></span>
+<span> <Icon icon="ic:round-play-arrow" />
+</span>
 </button>
+</div>
 </div>
 </template>
