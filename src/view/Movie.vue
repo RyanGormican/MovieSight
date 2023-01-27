8463d2b83d3f43de05e657c8652a0a25b83853eb
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 05:03:58 -0600

    a
    a

--- src/view/Movie.vue
+++ src/view/Movie.vue
@@ -8,7 +8,6 @@ const movieId = router.currentRoute.value.params.id
 const movie=ref(null)
 movie.value = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=c46280d39e34b3012975df9f8e6e9e70`).then(res => res.json())
 
-})
 const {
 title,
 overview: description,
