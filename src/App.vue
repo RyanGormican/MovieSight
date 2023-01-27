969292ce990932ef322b590c96e31e771a4af7c5
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 04:07:21 -0600

    a
    a

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,9 @@
 <script setup>
-  import { ref, onBeforeMount, defineAsyncComponent } from "vue"
-  import Navbar from "./components/Navbar.vue";
-  import MovieList from "./components/MovieList.vue";
-  const movies = ref([])
-  const bannerMovie = ref(null)
-
-  const AsyncBanner = defineAsyncComponent(() => {
-  return "./components/Banner.vue"
-  })
-  const findMovies = async () => {
-  movies.value = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=c46280d39e34b3012975df9f8e6e9e70")
-  .then(res => res.json())
-  .then(res => res.results)
-  }
-
-  const findRandom = (min, max) => {
-  return Math.floor(Math.random() * (max - min) + min)
-  }
-
-  onBeforeMount(async() => {
-  await findMovies()
-  bannerMovie.value = movies.value[findRandom(0, movies.value.length - 1)]
-  })
+  import { RouterView } from "vue-router"
 
 </script>
 <template>
-  <Navbar />
-  <AsyncBanner
-    :banner ="bannerMovie"
-/>
-  <MovieList
-    :movies="movies"
-/>
+
+  <RouterView />
 </template>
 
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,5 +1,38 @@
+<script setup>
+  import { ref, onBeforeMount, defineAsyncComponent } from "vue"
+  import Navbar from "../components/Navbar.vue"; 
+  import MovieList from "../components/MovieList.vue";
+  const movies = ref([])
+  const bannerMovie = ref(null)
+
+  const AsyncBanner = defineAsyncComponent(() => {
+  return "../components/Banner.vue"
+  })
+  const findMovies = async () => {
+  movies.value = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=c46280d39e34b3012975df9f8e6e9e70")
+  .then(res => res.json())
+  .then(res => res.results)
+  }
+
+  const findRandom = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min)
+  }
+
+  onBeforeMount(async() => {
+  await findMovies()
+  bannerMovie.value = movies.value[findRandom(0, movies.value.length - 1)]
+  })
+
+</script>
+
 <template>
 <div>
-Home
+  <Navbar />
+  <AsyncBanner
+    :banner ="bannerMovie"
+/>
+  <MovieList
+    :movies="movies"
+/>
 </div>
 </template
