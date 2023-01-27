8d870affeefffdecaaf4dd6e340a1a2faad60c47
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 05:39:11 -0600

    a
    a

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,6 +1,5 @@
 <script setup>
 import getImage from '../lib/getImage';
-import { Icon } from '@iconify/vue';
 import { RouterLink } from 'vue-router';
 const { movie } = defineProps(["movie"])
 const {
@@ -26,14 +25,14 @@ const {
 	<div class ="group-hover:opacity-100 transition-opacity opacity-0 absolute left-[-50%] top-[50%] translate-x-[50%] flex flex-col gap-2 w-full font-semibold text-white">
 		<div class="w-full flex-col items-center justify-center">
 		<button>
-			<Icon icon="ic:around-play-arrow" width="60" class="hover:text-emerald-600"/>
+			<p width="60" class="hover:text-emerald-600"> > </p>
 		</button>
 		</div>
 	</div>
 	<div class="absolute top-4 right-4 px-2 bg-emerald-700 py-1 rounded-md">
 	<p class="flex items-center gap-1 justify-center">
 		<span>{{ vote }}</span>
-		<Icon icon="material-symbols:star-rounded" color="white" />
+
 	</p>
 	</div>
 	<div>
