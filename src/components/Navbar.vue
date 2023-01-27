0faea6601848a8ae87ed2764912cbd7cc9958804
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 05:33:38 -0600

    a
    a

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,12 @@
 <script setup>
-import { Icon } from '@Iconify/vue';
-import { RouterLink } from 'vue-router';
+import { RouterLink }from 'vue-router';
 </script> 
 <template>
 <nav class="flex text-white items-center gap-20 absolute left-0 right-0 mx-6 py-4 top-4 bg-transparent border-b-2 border-neutral-400">
 	<RouterLink to="/" class="flex text-3xl items-center font-bold gap-2">
-	<Icon icon= "mingcute:movie-line" width = "40" />
 	<span> MovieSight</span>
 	</RouterLink>
+	</div>
 	<ul class="flex items-center gap-4 font-semibold">
 	</ul>
 </nav>
