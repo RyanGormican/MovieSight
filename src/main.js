cf9134d5944d88c0c97ee0fe58db7c7e6adec1c0
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 04:02:33 -0600

    a
    a

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import { router} from './routes';
 import './style.css'
 import 'flowbite';
 import App from './App.vue'
-createApp(App).mount('#app')
+createApp(App)
+.use(router) 
+.mount('#app')
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,2 +1,11 @@
 import Home from "../view/Home.vue"
 import Movie from "../view/Movie.vue"
+import { createRouter, createWebHistory  } from "vue-router"
+export const routes = [
+	{ path: "/", component: Home },
+	{ path: "/movies/:id", component: Movie },
+]
+const router = createRouter({
+history: createWebHistory()
+routes
+})
