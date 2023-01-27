170f81fcce788b5d03b9ac02739dcf7f62e5f528
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 01:39:23 -0600

    a
    a

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
   import { ref, onMounted } from "vue"
   const movies = ref(null)
-  const getMovies = async () => {
+  const findMovies = async () => {
    movies.value = await fetch("https://api.themoviedb.org/3/movie/550?api_key=c46280d39e34b3012975df9f8e6e9e70")
   .then(res => res.json{})
+  .then(res =>res.results)
  }
+ onMounted(async() => {
+ await findMovies()
+ })
 </script>
 <template>
   <h1> Hello World</h1>
