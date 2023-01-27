554d1b33ad2660e16c8ffa6dbd6e8ffe75881089
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 03:13:27 -0600

    a
    a

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,6 @@
 <template>
-<div>
-a
+<div class="flex text-white items-center gap-20 absolute left-0 right-0 mx-6 py-4 top-4 bg-transparent border-b-2 border-neutral-400">
+	<div>
+	</div>
 </div>
 </template>
