8e2fbe1b2222758ee89c75397676df2a4e10cc28
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 03:31:34 -0600

    a
    a

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -18,12 +18,14 @@ const {
 	<div class="relative group w-full h-full">
 	<img
 	:src="getImage(poster)"
+	class="w-full h-full transition rounded-md group-hover:blur-sm cursor-pointer"
 	/>
 	<div class ="group-hover:opacity-100 transition-opacity opacity-0 absolute left-[-50%] top-[50%] translate-x-[50%] flex flex-col gap-2 w-full font-semibold text-white">
-		
+		<div class="w-full flex-col items-center justify-center">
 		<button>
 			<Icon icon="ic:around-play-arrow" width="60" class="hover:text-emerald-600"/>
 		</button>
+		</div>
 	</div>
 	</div>
 </div>
