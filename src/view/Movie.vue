7d98ae476ce0715c473c6881c097c94d9fef01c9
RyanGormican <ryan.gormican@gmail.clom>
2023-01-27 04:32:22 -0600

    a
    a

--- src/view/Movie.vue
+++ src/view/Movie.vue
@@ -1,11 +1,10 @@
 <script setup>
 import { useRouter } from "vue-router"
-import { onMounted, ref } from "vue";
+import {  ref } from "vue";
 import getImage from '../lib/getImage"
 const router = useRouter()
 const movieId = router.currentRoute.value.params.id
 const movie=ref(null)
-onMounted(async() => {
 movie.value = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=c46280d39e34b3012975df9f8e6e9e70`).then(res => res.json())
 
 })
@@ -23,5 +22,10 @@ original_language: language
 :style="{
 backgroundImage= 'url(' +getImage(background)+')'
 }"
+>
+<div class="w-full h-full bg-gradient-to-r from-black to-transparent">
+
 </div>
+</div>
+
 </template>
