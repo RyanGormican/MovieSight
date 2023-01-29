28e57a45b17e7a89edf61580a6a9b95e1d362060
RyanGormican <ryan.gormican@gmail.clom>
2023-01-29 02:43:45 -0600

    detailed card full height list view reduced height

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -17,7 +17,7 @@
 	<RouterLink :to="`/movies/${id}`">
 		<div class="w-full h-full grid grid-rows-[4fr_1fr]">
 			<div class="relative group w-full h-full">
-				<img :src="getImage(poster)" class="w-full h-full transition rounded-md group-hover:blur-sm cursor-pointer" />
+				<img :src="getImage(poster)" class="w-full h-40% transition rounded-md group-hover:blur-sm cursor-pointer" />
 				<div class="group-hover:opacity-100 transition-opacity opacity-0 absolute left-[-50%] top-[50%] translate-x-[50%] flex flex-col gap-2 w-full font-semibold text-white">
 					<div class="w-full flex-col items-center justify-center">
 						<button>
