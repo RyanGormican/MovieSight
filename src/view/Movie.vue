4d7c3e41984e194becd9bbfc8b12715c292093c9
RyanGormican <ryan.gormican@gmail.clom>
2023-01-30 02:20:13 -0600

    Money reformatted, Added more details

--- src/view/Movie.vue
+++ src/view/Movie.vue
@@ -18,9 +18,12 @@
 		runtime,
 		original_language: language,
 		budget,
-		revenue
+		revenue,
+		tagline,
+		production_companies,
+		production_countries,
+		genres
 	} = movie.value
-	const country = movie.value.production_countries[0].iso_3166_1
 </script>
 <template>
 	<div class="h-screen w-screen grayscale-0" :style="{
@@ -33,6 +36,30 @@ backgroundImage: 'url(' +getImage(background)+')'
 					<h1 class="text-4xl font-semibold mb-4">{{ title }}</h1>
 					<p class="text-sm text-neutral-300 w-2/3">{{ description }}</p>
 					<div class="flex flex-col text-sm gap-2 mt-3">
+						<div class="flex items-center gap-2" v-if="tagline.length>0">
+							<Icon icon="emojione-monotone:left-speech-bubble" />
+							<span>{{ tagline }} </span>
+						</div>
+						<div class="flex items-center gap-2 ">
+							<div class="flex items-center gap-2" v-for="genre in genres">
+								<Icon icon="ph:sword" v-if="genre.name === 'Action'" />
+								<Icon icon="material-symbols:map" v-if="genre.name === 'Adventure'" />
+								<Icon icon="mdi:film-reel" v-if="genre.name === 'Animation'" />
+								<Icon icon="ri:emotion-laugh-fill" v-if="genre.name === 'Comedy'" />
+								<Icon icon="mdi:gun" v-if="genre.name === 'Crime'" />
+								<Icon icon="game-icons:drama-masks" v-if="genre.name === 'Drama'" />
+								<Icon icon="icon-park-outline:family" v-if="genre.name === 'Family'" />
+								<Icon icon="fa6-solid:wand-sparkles" v-if="genre.name === 'Fantasy'" />
+								<Icon icon="mdi:scroll-text-outline" v-if="genre.name === 'History'" />
+								<Icon icon="material-symbols:skull" v-if="genre.name === 'Horror'" />
+								<Icon icon="ion:musical-notes-sharp" v-if="genre.name === 'Music'" />
+								<Icon icon="ph:detective" v-if="genre.name === 'Mystery'" />
+								<Icon icon="mdi:alien" v-if="genre.name === 'Science Fiction'" />
+								<Icon icon="ri:knife-blood-line" v-if="genre.name === 'Thriller'" />
+								<Icon icon="healthicons:war" v-if="genre.name === 'War'" />
+								<span>{{ genre.name }} </span>
+							</div>
+						</div>
 						<div class="flex items-center gap-2">
 							<Icon icon="uiw:date" />
 							<span>{{ release_date }} </span>
@@ -47,17 +74,25 @@ backgroundImage: 'url(' +getImage(background)+')'
 						</div>
 						<div class="flex items-center gap-2" v-if="budget!== 0 && revenue !== 0">
 							<Icon icon="uil:money-bill" />
-							<span> $ {{ revenue }} </span>
+							<span> $ {{ revenue.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') }} </span>
 							<Icon icon="uil:money-bill-slash" />
-							<span> $ {{ budget }} </span>
+							<span> $ {{ budget.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,') }} </span>
 						</div>
 						<div class="flex items-center gap-2">
 							<Icon icon="ic:twotone-access-time-filled" color="white" />
 							<span>{{ runtime }} minutes</span>
 						</div>
-						<div class="flex items-center gap-2">
-							<img class="w-8" :src="`https://countryflagsapi.com/png/${country.toLowerCase()}`" crossorigin="anonymous" />
-							<span>{{country }} </span>
+						<div class="flex items-center gap-3 ">
+							<div class="flex items-center gap-2" v-for="company in production_companies">
+								<img class="w-8" :src="getImage(company.logo_path)" crossorigin="anonymous" v-if="getImage(company.logo_path)!='https://image.tmdb.org/t/p/original/null'"/>
+								<span>{{ company.name }} </span>
+							</div>
+						</div>
+						<div class="flex items-center gap-2 ">
+							<div class="flex items-center gap-2" v-for="country in production_countries">
+								<img class="w-8" :src="`https://countryflagsapi.com/png/${country.iso_3166_1.toLowerCase()}`" crossorigin="anonymous" />
+								<span>{{country.name }} </span>
+							</div>
 						</div>
 					</div>
 				</div>
